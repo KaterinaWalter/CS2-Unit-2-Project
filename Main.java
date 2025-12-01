aadd03708510762b7e66a3bd0182dac9a233498e
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-12-01 14:40:30 +0000

    starter code

--- Main.java
+++ Main.java
@@ -1,45 +1,42 @@
-import java.util.Scanner;
-
-public class Main 
-{
-
-   public static void main(String []args) 
-   {
-      System.out.println("Starting a new adventure...\n");
-      String command = ""; // stores the user's choices
-
-      // STEP #1: Describe the scenario scene
-      System.out.println("You are at a crossroads.");
-      // STEP #2: Get user command (prompt & take input)
-      command = getCommand("Do you take the path on the LEFT (l) or RIGHT (r)?");
-      // STEP #3: Process user command (choose a path)
-      if (command.equals("l")) 
-      {
-         System.out.println("You turned left.");
-      }
-      else if (command.equals("r")) 
-      {
-         System.out.println("You turned right.");
-      }
-      else 
-      {
-         System.out.println("Bad input. Press RUN to restart!");
-      }
+ import java.util.Scanner;
+ public class Main 
+ {
+    public static void main(String []args) 
+    {
+       System.out.println("Starting a new adventure...\n");
+       String command = ""; // stores the user's choices
       
-   } // END OF MAIN METHOD
+      // STEP #1: Describe the current scenario
+      System.out.println("You are at the Columbus Circle entrance to Central Park.");
+      // STEP #2: Get user command
+      command = getCommand("Do you go east (e) or north (n)?");
+      // STEP #3: Select a path based on the user's command
+      if ( command.equals("e") ) {
+         // NEXT BRANCH...
+         System.out.println("You reach the Carousel.");
+      }
+      else if ( command.equals("n") ) {
+         System.out.println("You see a wedding at Tavern on the Green");
+      }
+      else {
+         System.out.println("GAME OVER! You got lost in the Time Warner center shopping for hours. Press RUN to replay.");
+      }
 
-   /** Method to prompt & process input for the next command
-     * @param prompt to display to user, asks for specific input
-     * @return user's input command as a lowercase String
-   */
-   public static String getCommand(String prompt) 
-   {
-      System.out.println("\n▶︎▶︎▶︎ " + prompt);
-      Scanner scan = new Scanner(System.in);
-      String command = scan.nextLine().toLowerCase();
-      scan.close();
-      return command;
-   } // END OF METHOD
 
 
-} // END OF CLASS
+
+    } // END OF MAIN METHOD
+    
+    /** Method to prompt & process input for the next command
+      * @param prompt to display to user, asks for specific input
+      * @return user's input command as a lowercase String
+    */
+    public static String getCommand(String prompt) 
+    {
+       System.out.println("\n▶︎▶︎▶︎ " + prompt);
+       Scanner scan = new Scanner(System.in);
+       String command = scan.nextLine().toLowerCase();
+       scan.close();
+       return command;
+    } // END OF METHOD
+ } // END OF CLASS
